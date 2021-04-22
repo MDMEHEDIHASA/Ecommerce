29ae2d15cea6595ca3aa1aa96c758a8f342a6b8e
MD Mehedi Hasan <48857205+MDMEHEDIHASA@users.noreply.github.com>
2021-04-22 21:53:48 +0600

    Update HomeScreen.js

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -2,25 +2,29 @@ import React,{useEffect} from "react";
 import {useDispatch,useSelector} from 'react-redux';
 import Product from '../components/Product';
 import { Row, Col } from "react-bootstrap";
-// import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchAllProduct} from '../actions/index';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
 
-const HomeScreen = () => {
-  //const [products,setProducts] = useState([]);
+const HomeScreen = ({match}) => {
+  const keyword = match.params.keyword;
+  const pageNumber = match.params.pageNumber ||1;
   const dispatch = useDispatch();
   const productList = useSelector(state=> state.productList);
-  const {loading,error,products} = productList;
+  const {loading,error,products,page,pages} = productList;
   useEffect(()=>{
-    dispatch(fetchAllProduct());
-    console.log();
-    //props.fetchAllProduct();
-  },[dispatch]);
+    dispatch(fetchAllProduct(keyword,pageNumber));
+  },[dispatch,keyword,pageNumber]);
   
   
   return (
     <div>
+      <Meta/>
+      {!keyword ? <ProductCarousel/> : <Link to="/" className="btn btn-light">Go Back</Link>}
       <h1>Latest Product</h1>
       {loading ? <Loader/> : error ? (<Message>{error.message}</Message>) :  <Row>
         {products.map(product => (
@@ -28,8 +32,9 @@ const HomeScreen = () => {
             <Product product={product}></Product>
           </Col>
         ))}
-      </Row>}
-     
+      </Row>
+      }
+     <Paginate pages={pages} page={page} keyword={keyword ? keyword :''}></Paginate>
     </div>
   );
 };
